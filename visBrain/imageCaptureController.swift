424f79c42a46d25e355e1c7aa294ec71731f4734
Apple <apple@apples-macbook-pro.local>
2018-04-29 03:53:16 +0530

    //AppDevelopment

---
+++ visBrain/imageCaptureController.swift
@@ -0,0 +1,35 @@
+//
+//  imageCaptureController.swift
+//  visBrain
+//
+//  Created by Apple on 29/04/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class imageCaptureController: UIViewController {
+
+    @IBOutlet weak var liveVisionBtn: UIButton!
+    @IBOutlet weak var cameraBtn: UIButton!
+       @IBOutlet weak var galleryBtn: UIButton!
+    @IBAction func liveVisionAction(_ sender: Any) {
+    }
+    @IBAction func cameraAction(_ sender: Any) {
+    }
+    @IBAction func galleryAction(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
